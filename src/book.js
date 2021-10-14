7fb0a79294a76e1f991e41b2ec1918e8ef10f0a9
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-14 17:55:47 -0600

    Add edit and write book

--- src/book.js
+++ src/book.js
@@ -18,19 +18,38 @@ if (!reviews.includes(msg)) {
 return reviews;
 }
 
+//if the msgs are the same, don't add it
+//.includes() is a method that checks is messages are
+//already in a array
+
 function calculatePageCount(title) {
   var wordcount = title.length;
   return wordcount * 20;
 }
-//if the msgs are the same, don't add it
-//.includes() is a method that checks is messages are
-//already in a array
+
+function writeBook(title, char, genre) {
+  var book = {
+    title: title,
+    mainCharacter: char,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  };
+  return book;
+}
+
+function editBook (book) {
+  book.pageCount = book.pageCount * .75;
+}
+
+//should decrease pageCount to 3/4 of what it was.
+
+
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
   calculatePageCount,
-  // writeBook,
-  // editBook
+  writeBook,
+  editBook
 }
