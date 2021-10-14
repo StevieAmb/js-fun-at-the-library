3c6420383989b46e109538757de1dc6522fd1f9f
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-13 20:55:12 -0600

    Add build main character

--- src/book.js
+++ src/book.js
@@ -2,9 +2,18 @@ function createTitle(title) {
   return `The ${title}`;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+var mainCharacter = {
+     name: name,
+     age: age,
+     pronouns: pronouns
+  };
+  return mainCharacter;
+}
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
