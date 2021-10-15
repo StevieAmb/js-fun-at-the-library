ccc1c21d2c8c06b886031ddb97a042e13dbe6c45
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-15 16:43:20 -0600

    Add shelf and unshelf book

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,35 @@
 
+function shelfBook(book, shelf) {
+ if (shelf.length < 3) {
+ shelf.unshift(book);
+} else {
+ return shelf;
+ }
+}
+
+function unshelfBook(name, shelfArray) {
+for (var i = 0; i < shelfArray.length; i++) {
+if (shelfArray[i].title === name) {
+shelfArray.splice(i,1);
+  }
+ }
+}
+
+
+
+
+
+//It is taking in a name, and an array. It needs to see if
+//the name it is taking in matches the shelfArray[i].name
+//and if it does, it needs to remove that book.
+
+
+
+//if value === book.name, remove it.
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
