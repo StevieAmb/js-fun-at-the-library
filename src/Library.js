9716d37047ffa4ebe719d112987d40cbd4feb6ba
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-16 11:34:50 -0600

    Add check out library

--- src/Library.js
+++ src/Library.js
@@ -14,9 +14,22 @@ function addBook (library, name) {
   library.shelves[name.genre].push(name);
 }
 
-function checkoutBook (library, title, genre) {
-return `You have now checked out ${title} from ${library.name}`
+function checkoutBook (library, title, shelf) {
+for (var i = 0; i < library.shelves[shelf].length; i++)
+if (library.shelves[shelf][i].title === title) {
+  library.shelves[shelf].splice(i, 1);
+  return `You have now checked out ${title} from the ${library.name}`;
 }
+return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+}
+
+//if book IS on shelf return You have now checked out
+//IF book not on shelf, return different string
+// has to have the book on the shelf
+// IF book not on shelf, return different string
+// be aware of what books are on the shelf
+//title has to equal name of book on the shelf and has to be dynamic
+
 
 
 
