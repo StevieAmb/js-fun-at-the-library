5e5ce43fd31204fa3a83da8548dc520ea322301e
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-16 09:41:58 -0600

    Add create lib and add book

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,29 @@
+function createLibrary (name) {
+   var library = {
+     name: name,
+     shelves: {
+       fantasy: [],
+       fiction: [],
+       nonFiction: []
+     }
+   }
+   return library;
+}
+
+function addBook (library, name) {
+  library.shelves[name.genre].push(name);
+}
+
+function checkoutBook (library, title, genre) {
+return `You have now checked out ${title} from ${library.name}`
+}
+
+
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
