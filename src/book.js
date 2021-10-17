7277abeba6f3fbcb2dfb95dcf52e2379acc7d6ac
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-17 09:35:58 -0600

    Add book test and refactoring

--- src/book.js
+++ src/book.js
@@ -3,50 +3,59 @@ function createTitle(title) {
 }
 
 function buildMainCharacter(name, age, pronouns) {
-var mainCharacter = {
-     name: name,
-     age: age,
-     pronouns: pronouns
-  };
+ var mainCharacter = {
+  name: name,
+  age: age,
+  pronouns: pronouns
+ };
   return mainCharacter;
 }
 
 function saveReview(msg, reviews) {
-if (!reviews.includes(msg)) {
+ if (!reviews.includes(msg)) {
   reviews.push(msg);
-}
-return reviews;
+ }
+ return reviews;
 }
 
-/*function saveReview(msg, reviews) {
+function saveReview(msg, reviews) {
+ var isUnique = true;
   for (var i = 0; i < reviews.length; i++) {
-    if (msg !== reviews[i]) {
-      reviews.push(msg);
-    }
-  }
-}*/
+   if (msg === reviews[i]) {
+    isUnique = false;
+   }
+ }
+   if (isUnique) {
+    reviews.push(msg)
+   }
+}
+
+//Use outside variable as counter when going through
+//a for loop.
+
 
 //if the msgs are the same, don't add it
 //.includes() is a method that checks is messages are
 //already in a array
 
 function calculatePageCount(title) {
-  var wordcount = title.length;
+ var wordcount = title.length;
   return wordcount * 20;
 }
 
 function writeBook(title, char, genre) {
-  var book = {
-    title: title,
-    mainCharacter: char,
-    pageCount: calculatePageCount(title),
-    genre: genre
-  };
-  return book;
+ return {
+  title: title,
+  mainCharacter: char,
+  pageCount: calculatePageCount(title),
+  genre: genre
+ };
 }
 
+//Try to use different names from properties and parameters
+
 function editBook (book) {
-  book.pageCount = book.pageCount * .75;
+ book.pageCount = book.pageCount * .75;
 }
 
 //should decrease pageCount to 3/4 of what it was.
