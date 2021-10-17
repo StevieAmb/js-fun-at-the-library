b9feb5e17d146e26149e7590c7a22e721c7ece8d
Stevie Ambroise <s.ambroise@protonmail.com>
2021-10-17 09:42:03 -0600

    Add shelf test and shelf refactoring

--- src/shelf.js
+++ src/shelf.js
@@ -1,21 +1,21 @@
 
 function shelfBook(book, shelf) {
  if (shelf.length < 3) {
- shelf.unshift(book);
-} else {
- return shelf;
+  shelf.unshift(book);
+ } else {
+  return shelf;
  }
 }
 
 function unshelfBook(name, shelfArray) {
-for (var i = 0; i < shelfArray.length; i++) {
-if (shelfArray[i].title === name) {
-shelfArray.splice(i,1);
+ for (var i = 0; i < shelfArray.length; i++) {
+  if (shelfArray[i].title === name) {
+   shelfArray.splice(i,1);
   }
  }
 }
 
-function listTitles(shelf) {
+/*function listTitles(shelf) {
 var titles = ""
 for (var i = 0; i < shelf.length; i++) {
 if (shelf.length-1 === i) {
@@ -25,15 +25,23 @@ titles = titles + shelf[i].title + ", "
 }
 }
 return titles;
+}*/
+
+function listTitles(shelf) {
+ var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.join(", ")
 }
 
 function searchShelf(shelf, name) {
-for (var i = 0; i < shelf.length; i++) {
+ for (var i = 0; i < shelf.length; i++) {
   if (shelf[i].title === name) {
     return true;
   }
-}
-return false;
+ }
+  return false;
 }
 //create list of all titles on a shelf
 
